d4d2554714aa2409380a3a43cd769a26b735c2b2
MikeyZat <41756225+MikeyZat@users.noreply.github.com>
2018-10-19 17:18:42 +0200

    Update 1.cpp

--- zestaw3/1.cpp
+++ zestaw3/1.cpp
@@ -1,1 +1,23 @@
-xD
+#include <iostream>
+#include<string>
+using namespace std;
+
+char znak(int liczba){
+    if(liczba<10){
+        return char(liczba+48);
+    }
+    return char(liczba+55);
+}
+
+int main()
+{
+    int podstawa,liczba;
+    string nowaLiczba="";
+    cin>>podstawa>>liczba;
+    
+    while(liczba>0){
+        nowaLiczba=znak(liczba%podstawa)+nowaLiczba;
+        liczba/=podstawa;
+    }
+    cout<<nowaLiczba;
+}
