5098d31cc9137a3e6bc0db6c2fed1af11e95f8e0
MikeyZat <41756225+MikeyZat@users.noreply.github.com>
2018-11-05 18:18:10 +0100

    added comments

--- zestaw4/4.cpp
+++ zestaw4/4.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 const int N = 5;
-
+//since they told me to return both co-ordinates
 struct element{
     int rowIndex;
     int columnIndex;
@@ -12,16 +12,18 @@ element function(int tab[N][N]){
     int sumOfColumn[N];
     int sumOfRow[N];
     for(int j=0;j<N;j++)sumOfRow[j]=sumOfColumn[j]=0;
-    
+    //calculating sum of each row and column
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
             sumOfRow[i]+=tab[i][j];
             sumOfColumn[j]+=tab[i][j];
         }
     }
+    //creating our candidate 
     element elem;
     elem.rowIndex=0;
     elem.columnIndex=0;
+    //since we need max quotient, we would like to maximalize numerator and minimalize denominator 
     for(int i=1;i<N;i++){
         if(sumOfRow[i]<sumOfRow[elem.rowIndex])
         elem.rowIndex=i;
