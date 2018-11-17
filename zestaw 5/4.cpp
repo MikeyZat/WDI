330e1807aa82c319009be738037f937a9f611691
MikeyZat <41756225+MikeyZat@users.noreply.github.com>
2018-11-17 21:33:08 +0100

    finished

--- zestaw 5/4.cpp
+++ zestaw 5/4.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
+const int SIZE = 10;
 
 struct ulamek{
     int l;
@@ -30,20 +31,31 @@ void skroc(ulamek &l){
     l.m/=b;
 }
 
-int policz(ulamek t[100]){
+ulamek dodaj(ulamek l1, ulamek l2){
+    ulamek wynik;
+    wynik.l=l1.l*l2.m + l2.l*l1.m;
+    wynik.m=l1.m*l2.m;
+    skroc(wynik);
+    return wynik;
+}
+
+ulamek pomnoz(ulamek l1, ulamek l2){
+    ulamek wynik;
+    wynik.l=l1.l*l2.l;
+    wynik.m=l1.m*l2.m;
+    skroc(wynik);
+    return wynik;
+}
+
+int policz(ulamek t[SIZE]){
     int ilA=0,ilG=0;
     
-    ulamek r,q,dwa;
+    ulamek dwa;
     dwa.l=2;
     dwa.m=1;
-    r.l=0;
-    r.m=1;
-    q.l=1;
-    r.m=1;
-    int i=1;
     bool wA=false;
     bool wG=false;
-    for(int i=1;i<99;i++){
+    for(int i=1;i<SIZE-1;i++){
         if(porownaj(pomnoz(t[i],dwa),dodaj(t[i-1],t[i+1]))){
             if(!wA)
                 ilA++;
@@ -60,7 +72,6 @@ int policz(ulamek t[100]){
             wG=false;
         }
     }
-    
     if(ilA>ilG){
         return 1;
     }else if(ilA==ilG){
@@ -69,17 +80,13 @@ int policz(ulamek t[100]){
         return -1;
     }
 }
-                
-    
-            
-}
-        
-
-
-
 int main()
 {
-    ulamek t[100];
+    ulamek t[SIZE];
+   for(int i=0;i<SIZE;i++){
+       cin>>t[i].l>>t[i].m;
+   }
    
+   cout<<policz(t);
   
 }
