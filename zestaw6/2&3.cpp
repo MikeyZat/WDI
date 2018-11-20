7fca96c34d2e49c0877d4dba864e141485d1d5e5
MikeyZat <41756225+MikeyZat@users.noreply.github.com>
2018-11-20 18:58:43 +0100

    Create 2&3.cpp

---
+++ zestaw6/2&3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+const int N=5;
+
+bool canBeWeighted(int current, const int weight, int idx, int tab[N]){    
+    if(current == weight)
+        return true;
+    if(idx >= N)
+        return false;
+    if(canBeWeighted(current+tab[idx], weight, idx+1, tab) ||canBeWeighted(current-tab[idx],weight,idx+1,tab)){
+        cout<<tab[idx]<<" ";
+        return true;
+        }else{
+            return canBeWeighted(current, weight, idx+1, tab);   
+        }
+}
+int main()
+{
+    int t[N];
+    for(int i=0;i<N;i++){
+        cin>>t[i];
+    }
+    int n;
+    cin>>n;
+    cout<<endl<<canBeWeighted(0,n,0,t);
+}
