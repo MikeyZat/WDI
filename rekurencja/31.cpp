f900b011078e5c2d355526ec24e304f71e9f8c03
Mikołaj Zatorski <41756225+MikeyZat@users.noreply.github.com>
2018-12-10 22:58:44 +0100

    Update 31.cpp

--- rekurencja/31.cpp
+++ rekurencja/31.cpp
@@ -2,24 +2,23 @@
 #include <string>
 #include <cmath>
 using namespace std;
-int max;
 const int N=10;
-
-void Nka(int t[N],const int n, const int suma, int s, int il, int i){    
+int maks;
+ void Nka(int t[N],const int n, const int suma, int s, int il, int i){    
     if(s == suma && il==n)
-        max++;
+        maks++;
     if(i==N)                    
         return;
     Nka(t, n, suma, s+t[i],il+1,i+1);
     Nka(t, n, suma, s,il,i+1);
 }
-
-int main()
+ int main()
 {   
-    max=0;
+    maks=0;
     int n,suma;
     int t[N];
     for(int i=0;i<N;i++)cin>>t[i];
     cin>>suma>>n;
-    
+    Nka(t,n,suma,0,0,0);
+    cout<<maks;    
 }
